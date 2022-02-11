3becb35702ba1e8f9d284afebaf8c1c9a2173d8f
Sushmoy_Rahul <45335164+JUCSERahull337@users.noreply.github.com>
2022-02-11 22:47:42 +0600

    Add files via upload

--- test_search.py
+++ test_search.py
@@ -1,16 +1,16 @@
 import unittest
 import dictionary
 import os
-class TestTextToSign(unittest.TestCase):
+class TestSearch(unittest.TestCase):
 
 	"""
 	  This is a  class whose instances are single test cases.It has a by default method run tests
       By default, the test code itself should be placed in a method named 'runTest'.
 	"""
 
-	def test_TextInput(self):
+	def test_Input(self):
 		"""
-		This function is going to match the text input (be careful) with the gif (be careful.gif)
+		This function is going to match the text input (be careful) with the image (be careful.image)
 		and after running test case has it passed or failed , will show it.
 		:param: self . This is going to create a instance of it's own and run the test case.
 		:return:OK(passed) or FAILED .
